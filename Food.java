587827c89445f92eae9fefe3e7de5017e4d2c88d
m3mor1ze <daniladaniladanilin@gmail.com>
2023-10-22 17:41:34 +0300

    Lab1

---
+++ Food.java
@@ -0,0 +1,25 @@
+public abstract class Food implements Consumable, Nutritious, PrintInfo {
+    String name = null;
+
+    public Food(String name) {
+        this.name = name;
+    }
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Food)) return false;
+        if (name == null || ((Food) obj).name == null) return false;
+        return name.equals(((Food) obj).name);
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,92 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws Exception {
+        Food[] breakfast = new Food[20];
+        boolean ifsort = false;
+        boolean ifcalories = false;
+
+        for (String arg : args) {
+            if (arg.matches("(^|\\s)-sort\\b(\\s|\\n|$)")) {
+                ifsort = true;
+                continue;
+            }
+            if (arg.matches("(^|\\s)-calories\\b(\\s|\\n|$)")) {
+                ifcalories = true;
+                continue;
+            }
+            if (arg.matches("\\-.*")) {
+                System.out.println("No such params, skip");
+            }
+        }
+
+        int itemsSoFar = 0;
+        Integer calories = 0;
+        for (String arg : args) {
+            String[] parts = arg.split("/");
+
+            if (parts[0].equals("Sandwich") || parts[0].equals("Dessert") || parts[0].equals("Cocktail")) {
+                Class<?> foodClass = Class.forName(parts[0]);
+
+                Food food = (Food) foodClass.getDeclaredConstructor(String.class, String.class).newInstance(parts[1], parts[0]);
+
+                breakfast[itemsSoFar] = food;
+                if (ifcalories)
+                    calories += food.calculateCalories();
+            } else {
+                System.out.println("Class not found");
+            }
+            itemsSoFar++;
+        }
+
+        for (Food item : breakfast) {
+            if (item != null) {
+                item.consume();
+            } else {
+                break;
+            }
+        }
+
+        if (ifcalories) {
+            System.out.println("Calories = " + calories);
+        }
+
+        if (ifsort) {
+            Arrays.sort(breakfast, Collections.reverseOrder(new Comparator<Food>() {
+                @Override
+                public int compare(Food first, Food second) {
+                    if (first == null && second == null) {
+                        return 0;
+                    }
+                    if (first == null) {
+                        return 1;
+                    }
+                    if (second == null) {
+                        return -1;
+                    }
+
+                    if (first.equals(second)) {
+                        return 0;
+                    }
+
+                    int caloriesCompare = first.calculateCalories() - second.calculateCalories();
+                    if (caloriesCompare != 0) {
+                        return caloriesCompare;
+                    }
+
+                    return first.getName().compareTo(second.getName());
+                }
+            }));
+
+            for (Food item : breakfast) {
+                if (item != null) {
+                    item.info();
+                }
+            }
+        }
+        System.out.println("Всего хорошего!");
+    }
+}
